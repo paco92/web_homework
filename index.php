100b8e397d932b82d9c6a05bdc7b6165e2381270
François HERVE <herve@d17-l428-pc-10.ipa.iisg.agh.edu.pl>
2018-11-06 10:47:46 +0100

    grfdw

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Hello World</title>
+</head "text align: center; font-size: 50px;">
+<body>
+<?php
+echo  "<h1>Friend Book</h1>";
+<form action="index.php" method="get">
+Name: <input type="text" name="name">
+<input type="submit">
+</form>
+?>
+
+
+</body>
+</html>
